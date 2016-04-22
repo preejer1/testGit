4c4bc7ce771a7f0da42f51e61edbc701a4064a72
gpal11266@naver.com <neopad@neopad-PC>
2016-04-22 18:25:04 +0900

    ddddd

--- hyTest/app.js
+++ hyTest/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-haha1
+haha1 success!!!!!
 app.use('/', routes);
 app.use('/users', users);
 
