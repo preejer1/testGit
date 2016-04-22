d4c00182035be9690379a83991160092f419a684
gpal1126 <user@user-PC>
2016-04-22 18:29:00 +0900

    qwer

--- hyTest/app.js
+++ hyTest/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-hohohohohohoh success
+니가뭔데 날 막아
 app.use('/', routes);
 app.use('/users', users);
 
@@ -31,7 +31,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
 // error handlers
 
 // development error handler
